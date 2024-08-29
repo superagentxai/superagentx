9fd7fe5b6530719882cc0dae4cb05d4687816d58
bala <balamurugan@decisionforce.io>
2024-08-29 18:40:00 +0530

    assert

--- tests/test_content_creater.py
+++ tests/test_content_creater.py
@@ -2,7 +2,7 @@ import os
 from agentx.handler.content_creator import ContentCreatorHandler
 from langchain_openai import ChatOpenAI
 
-os.environ["OPENAI_API_KEY"] = "<OPENAI_API_KEY>"
+os.environ["OPENAI_API_KEY"] = "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"
 content_creator_handler = ContentCreatorHandler(
     prompt="Create the digital marketing content",
     llm=ChatOpenAI(model="gpt-4o")
@@ -11,4 +11,5 @@ content_creator_handler = ContentCreatorHandler(
 
 def test_handle_1():
     result = content_creator_handler.handle(action="TEXT")
+    assert "digital marketing" in result
 
