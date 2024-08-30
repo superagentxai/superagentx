e28a39e3db50fbf0e5e43d7e4632830a2c82ef97
Praveena Talluri <praveenatalluri@Praveenas-MacBook-Pro.local>
2024-08-30 18:30:40 +0530

    write the test case

--- agentx/handler/aws_s3/__init__.py
+++ agentx/handler/aws_s3/__init__.py
@@ -3,10 +3,9 @@ from enum import Enum
 from typing import Any
 
 import boto3
-from botocore.config import Config
 from botocore.exceptions import NoCredentialsError, ClientError
-from poetry.utils.helpers import Downloader, download_file
 
+from agentx.exceptions import InvalidType
 from agentx.handler.aws_s3.exceptions import ListFilesFailed
 from agentx.handler.base import BaseHandler
 
@@ -18,7 +17,7 @@ class AWSS3HandlerEnum(str, Enum):
     LIST_BUCKET = "list_bucket"
     UPLOAD_FILE = "upload_file"
     DOWNLOAD_FILE = "download_file"
-    LIST_FILES = "list_files"
+
 
 
 class AWSS3Handler(BaseHandler):
@@ -42,33 +41,23 @@ class AWSS3Handler(BaseHandler):
     def handle(self, *, action: str | Enum, **kwargs) -> Any:
         match action:
             case AWSS3HandlerEnum.LIST_BUCKET:
-                self.list_buckets(**kwargs)
+                self.list_bucket()
             case AWSS3HandlerEnum.UPLOAD_FILE:
                 self.upload_file(**kwargs)
             case AWSS3HandlerEnum.DOWNLOAD_FILE:
                 self.download_file(**kwargs)
-            case AWSS3HandlerEnum.LIST_FILES:
-                self.list_files()
+            case _:
+                raise InvalidType(f"Invalid action {action}")
 
-    def list_buckets(
-            self,
-            prefix: str | None = None,
-            delimiter: str = "/"
-    ):
+    def list_bucket(self):
         try:
-            res = self._storage.list_buckets(
-                Bucket=self.bucket_name,
-                Delimiter=delimiter,
-                Prefix=prefix
-            )
+            res = self._storage.list_buckets()
             if res and isinstance(res, dict):
                 return res.get('Contents')
         except (NoCredentialsError, ClientError) as ex:
             _msg = "Error listing files"
             logger.error("Error listing files", exc_info=ex)
             raise ListFilesFailed(ex)
-
-
 
     def upload_file(self, file_name, object_name=None):
         if object_name is None:
@@ -79,13 +68,10 @@ class AWSS3Handler(BaseHandler):
             return True
         except FileNotFoundError:
             logger.error(f"The file '{file_name}' was not found.")
-            return False
-        except NoCredentialsError:
-            logger.error("Credentials not available.")
-            return False
+        except NoCredentialsError as ex:
+            logger.error(f"Credentials not available.{ex}")
         except ClientError as e:
             logger.error(f"Client error: {e}")
-            return False
 
     def download_file(self, object_name, file_name=None):
         if file_name is None:
@@ -100,17 +86,3 @@ class AWSS3Handler(BaseHandler):
         except ClientError as e:
             logger.error(f"Client error: {e}")
             return False
-
-    def list_files(self):
-        try:
-            response = self._storage.list_objects_v2(Bucket=self.bucket_name)
-            for item in response.get('Contents', []):
-                logger.info(f"File found: {item['Key']}")
-            return response.get('Contents', [])
-        except NoCredentialsError:
-            logger.error("Credentials not available.")
-            return []
-        except ClientError as e:
-            logger.error(f"Client error: {e}")
-            return []
-
--- tests/test_aws_s3.py
+++ tests/test_aws_s3.py
@@ -1,30 +1,27 @@
+from fileinput import filename
+
 from agentx.handler.aws_s3 import AWSS3Handler
 import os
 s3_handler = AWSS3Handler(
 
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    bucket_name="ocppbeta-artifacts",
-    region_name="ap-south-1"
+    bucket_name="<BUCKET_NAME>",
+    region_name="eu-central-1"
 
 
 
 )
-def test_s3_handler_1():
-    #res = s3_handler.list_buckets()
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+def test_s3_handler_upload():
+    s3_handler.handle(action="upload_file",file_name="<file_path>")
 
-    # Define the .txt file name
-    txt_upload_file_name = 'test.txt'
+def test_s3_handler_list_bucket():
+    s3_handler.handle(action="list_bucket")
 
-    # Full path to the .txt file
-    file_path = os.path.join(current_directory, txt_upload_file_name)
-    #uncomment to test upload files
-    res=s3_handler.upload_file(file_path,"test_new_folder/test_sub_folder/test.txt")
-    #uncomment to test download files
-    # download_res=s3_handler.download_file("test_new_folder/test_sub_folder/test.txt","E:/praveena_aws/testterdownload.txt")
-    #uncomment to test list files
-    #list_files = s3_handler.list_files()
-    #print(list_files)
+def test_s3_handler_download():
+    s3_handler.handle(action="download_file",object_name="<file_path>")
+
+
+
 
 
