8092d2c35f08d335456921b77b2f9d23f451ab12
bala <balamurugan@decisionforce.io>
2024-09-12 11:52:49 +0530

    vector database base

--- agentx/handler/vector_database/base.py
+++ agentx/handler/vector_database/base.py
@@ -6,20 +6,26 @@ class BaseVectorDatabase(metaclass=ABCMeta):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod
     def create(self, *args, **kwargs):
         raise NotImplementedError
 
+    @abstractmethod
     def index(self, *args, **kwargs):
         raise NotImplementedError
 
+    @abstractmethod
     def search(self, *args, **kwargs):
         raise NotImplementedError
 
+    @abstractmethod
     def delete(self, *args, **kwargs):
         raise NotImplementedError
 
+    @abstractmethod
     def update(self, *args, **kwargs):
         raise NotImplementedError
 
+    @abstractmethod
     def exists(self, *args, ** kwargs):
         raise NotImplementedError
