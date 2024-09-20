e49c1c319b67fea48259107d53a317529f6e442c
vetharupini <vetharupini@decisionfacts.io>
2024-09-20 13:03:26 +0530

    added commands

--- tests/handlers/test_elastic_search.py
+++ tests/handlers/test_elastic_search.py
@@ -26,18 +26,21 @@ def elasticsearch_client_init() -> ElasticsearchHandler:
 
 class TestElasticsearch:
 
+    #Test sync elasticsearch handler - search method
     def test_elasticsearch_search(self, elasticsearch_client_init: ElasticsearchHandler):
         elasticsearch = elasticsearch_client_init.handle(action="search",
                                                          index_name="index_name",
                                                          )
         assert isinstance(elasticsearch, object)
 
+    # Test async elasticsearch handler - search method
     async def test_elasticsearch_asearch(self, elasticsearch_client_init: ElasticsearchHandler):
         elasticsearch = await elasticsearch_client_init.ahandle(action="search",
                                                                 index_name="index_name",
                                                                 )
         assert isinstance(elasticsearch, object)
 
+    # Test sync elasticsearch handler - create method
     def test_elasticsearch_create(self, elasticsearch_client_init: ElasticsearchHandler):
         elasticsearch = elasticsearch_client_init.handle(action="create",
                                                          index_name="index_name",
@@ -52,6 +55,7 @@ class TestElasticsearch:
                                                          )
         assert isinstance(elasticsearch, object)
 
+    # Test async elasticsearch handler - create method
     async def test_elasticsearch_acreate(self, elasticsearch_client_init: ElasticsearchHandler):
         elasticsearch = await elasticsearch_client_init.ahandle(action="create",
                                                           index_name="index_name",
