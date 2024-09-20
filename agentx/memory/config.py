055a438ab9812363dbebc363bb751a473d6869ad
Prabhu Raghav <raghavprabhu06@gmail.com>
2024-09-20 06:48:23 +0530

    Memory based added

---
+++ agentx/memory/config.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+from agentx.memory.setup import mem_dir
+import os
+
+
+class MemoryConfig(BaseModel):
+
+    db_path: str = Field(
+        description="Path to the database",
+        default=os.path.join(mem_dir, "history.db"),
+    )
+
+    version: str = Field(
+        description="The version of the API",
+        default="v1.0",
+    )
+    custom_prompt: Optional[str] = Field(
+        description="Custom prompt for the memory",
+        default=None,
+    )
