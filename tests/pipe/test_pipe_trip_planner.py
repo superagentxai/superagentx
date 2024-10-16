40e1e10a027a25d21eaa64b6e2fcb6bb67a77d76
bala <balamurugan@decisionforce.io>
2024-10-16 13:42:55 +0530

    added

--- tests/pipe/test_pipe_trip_planner.py
+++ tests/pipe/test_pipe_trip_planner.py
@@ -1,10 +1,24 @@
+import logging
+
 import pytest
 
 from agentx.agent.agent import Agent
 from agentx.agent.engine import Engine
 from agentx.handler.ai_handler import AIHandler
+from agentx.io import IOConsole
 from agentx.llm import LLMClient
+from agentx.pipe import AgentXPipe
 from agentx.prompt import PromptTemplate
+from agentx.utils.console_color import ConsoleColorType
+
+logger = logging.getLogger(__name__)
+
+'''
+ Run Pytest:  
+   1.pytest -s --log-cli-level=INFO tests/pipe/test_ai_pipe.py::TestIOConsolePipe::test_ai_spamfilter
+   2.pytest -s --log-cli-level=INFO tests/pipe/test_ai_pipe.py::TestIOConsolePipe::test_writer
+   3.pytest -s --log-cli-level=INFO tests/pipe/test_ai_pipe.py::TestIOConsolePipe::test_scorer
+'''
 
 
 @pytest.fixture
@@ -18,7 +32,7 @@ def agent_client_init() -> dict:
 
 class TestTripPlannerPipe:
 
-    async def test_trip_planner(self, agent_client_init):
+    async def test_city_selection_agent(self, agent_client_init):
         llm_client: LLMClient = agent_client_init.get('llm')
         ai_handler = AIHandler(
             llm=llm_client,
@@ -32,10 +46,21 @@ class TestTripPlannerPipe:
             prompt_template=prompt_template
         )
         city_selection_agent = Agent(
-            name="Ecom Agent",
-            goal="Get me the best search results",
-            role="You are the best product searcher",
+            name="City Selection Agent",
+            role='City Selection Expert',
+            goal='Select the best city based on weather, season, and prices',
             llm=llm_client,
             prompt_template=prompt_template,
             engines=[ai_engine]
         )
+        pipe = AgentXPipe(
+            agents=[city_selection_agent]
+        )
+
+        io_console = IOConsole()
+        while True:
+            await io_console.write(ConsoleColorType.CYELLOW2.value, end="")
+            query_instruction = await io_console.read("User: ")
+            result = await pipe.flow(query_instruction)
+            await io_console.write(ConsoleColorType.CGREEN2.value, end="")
+            await io_console.write(f"Assistant: {result}", flush=True)
