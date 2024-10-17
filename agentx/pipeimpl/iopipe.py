91dc7a56ea807c8c0571ac47f148a40c7c0bcbd1
Prabhu Raghav <raghavprabhu06@gmail.com>
2024-10-17 05:49:59 +0530

    agentx variable spell updated

--- agentx/pipeimpl/iopipe.py
+++ agentx/pipeimpl/iopipe.py
@@ -6,11 +6,11 @@ class IOPipe:
 
     def __init__(
             self,
-            agetnx_pipe: AgentXPipe,
+            agentx_pipe: AgentXPipe,
             read_prompt: str | None = None,
             write_prompt: str | None = None
     ):
-        self.agetnx_pipe = agetnx_pipe
+        self.agentx_pipe = agentx_pipe
         self.io_console = IOConsole(
             read_phrase=read_prompt,
             write_phrase=write_prompt
@@ -20,13 +20,14 @@ class IOPipe:
         while True:
             query = await self.io_console.read()
             with await self.io_console.status("[bold yellow] Searching...\n"):
-                pipe_result = await self.agetnx_pipe.flow(query_instruction=query)
+                pipe_result = await self.agentx_pipe.flow(query_instruction=query)
                 if pipe_result:
                     goal_result = pipe_result[-1]
                     await self.io_console.write(f'\n[bold cyan]Result:[/bold cyan] {goal_result.result}\n')
                     await self.io_console.write(f'\n[bold cyan]Reason[/bold cyan]:')
                     await self.io_console.json(data=goal_result.result)
-                    await self.io_console.write(f'\n\n[bold cyan]Goal Satisfied[/bold cyan]: {goal_result.is_goal_satisfied}\n')
+                    await self.io_console.write(
+                        f'\n\n[bold cyan]Goal Satisfied[/bold cyan]: {goal_result.is_goal_satisfied}\n')
                 else:
                     await self.io_console.write("\nNo results found!\n")
             await self.io_console.rule('[bold green]End')
