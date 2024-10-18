3794f493870915aef459dc8c433893118754f0d3
Arul Vivek <arulvivek@Aruls-MacBook-Pro.local>
2024-10-18 19:26:50 +0530

    commets resolved

--- superagentx/handler/atlassian/confluence.py
+++ superagentx/handler/atlassian/confluence.py
@@ -90,21 +90,8 @@ class ConfluenceHandler(BaseHandler):
             logger.error(message, exc_info={ex})
             raise Exception(message)
 
-    async def download_pages(self):
-        try:
-            result = await sync_to_async(
-                self._connection.export_page,
-                page_id=959938561
-            )
-            return result
-        except Exception as ex:
-            message = f"Error While getting confluence spaces! {ex}"
-            logger.error(message, exc_info={ex})
-            raise Exception(message)
-
     def __dir__(self):
         return (
             'get_all_spaces',
             'get_pages_spaces',
-            'download_pages'
         )
--- tests/handlers/test_atlassian_handler.py
+++ tests/handlers/test_atlassian_handler.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
     10. pytest --log-cli-level=INFO tests/handlers/test_atlassian_handler.py::TestAtlassian::test_active_sprint_filter_issues_by_status
     11. pytest --log-cli-level=INFO tests/handlers/test_atlassian_handler.py::TestAtlassian::test_get_all_spaces
     12. pytest --log-cli-level=INFO tests/handlers/test_atlassian_handler.py::TestAtlassian::test_get_pages_spaces
-    13. pytest --log-cli-level=INFO tests/handlers/test_atlassian_handler.py::TestAtlassian::test_download_pages
+    
 '''
 
 
@@ -157,9 +157,4 @@ class TestAtlassian:
         logger.info(f"Pages: {res}")
         assert isinstance(res, dict)
         assert len(res) > 0
-
-    async def test_download_pages(self, confluence_client_init: ConfluenceHandler):
-        res = await confluence_client_init.download_pages()
-        logger.info(f"Page: {res}")
-        assert res
 
