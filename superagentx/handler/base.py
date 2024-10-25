02734eaa5167bded9eb2fe8c55446424f4f8f054
Syed <syed@decisionforce.io>
2024-10-25 18:34:48 +0530

    fixed merge conflicts

--- superagentx/handler/base.py
+++ superagentx/handler/base.py
@@ -11,7 +11,7 @@ class BaseHandler(abc.ABC):
 
     def _get_tools(self) -> list[str]:
         self.tools = []
-        for _member in inspect.getmembers(self):
+        for _name, _member in inspect.getmembers(self):
             if inspect.isfunction(_member) or inspect.ismethod(_member):
                 if getattr(_member, '_is_handler_tool') and _member._is_handler_tool:
                     self.tools.append(_member.__name__)
