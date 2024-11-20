6d5588073090df47f201b7c843ca84853bc2d403
Syed <syed@decisionforce.io>
2024-11-20 18:48:02 +0530

    added format

--- superagentx/config.py
+++ superagentx/config.py
@@ -7,6 +7,7 @@ def is_verbose_enabled():
     verbose = os.environ.get('DEBUGGING') or os.environ.get('VERBOSE')
     if verbose and verbose.lower() in ('1', 'true'):
         logging.basicConfig(
+            format='%(name)s - %(levelname)s - %(message)s',
             stream=sys.stdout,
             level=logging.DEBUG,
             force=True
