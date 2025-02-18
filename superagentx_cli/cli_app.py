6c284062eee74a9f342cc75a2f6de21d64f1157f
Syed Habib <syed.habib53@gmail.com>
2025-02-18 17:12:49 +0530

    app config error

--- superagentx_cli/cli_app.py
+++ superagentx_cli/cli_app.py
@@ -1,8 +1,10 @@
 import json
+import sys
 from typing import Annotated, Optional
 
 import typer
 from superagentx_cli.cli import CliApp
+from superagentx_cli.exceptions import AppConfigError
 
 app = typer.Typer(name='Superagentx-App')
 
@@ -19,11 +21,15 @@ def main(
 ):
     with open(app_config_path, 'r') as fobj:
         app_config = json.load(fobj)
-        cli_app = CliApp(
-            app_config=app_config,
-            app_dir_path=app_dir_path
-        )
-        cli_app.create_project()
+        try:
+            cli_app = CliApp(
+                app_config=app_config,
+                app_dir_path=app_dir_path
+            )
+            cli_app.create_project()
+        except (AppConfigError, Exception) as ex:
+            sys.stderr(f"Superagentx app creation failed!\n{ex}")
+            sys.exit(1)
 
 if __name__ == '__main__':
     app()
